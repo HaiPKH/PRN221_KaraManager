d8590c9bd52ab30c758e4c8360acaf5b92f1d6a3
HaiPKH <100670221+HaiPKH@users.noreply.github.com>
2023-03-27 00:11:47 +0700

    Added KaraChatServer manually

---
+++ KaraChatServer/Hubs/KaraChatHub.cs
@@ -0,0 +1,32 @@
+﻿using KaraManager.Model;
+using Microsoft.AspNetCore.SignalR;
+using System.Windows;
+
+namespace KaraChatServer.KaraChatHub
+{
+    public class KaraChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            //MessageBox.Show("Connected", "Wutdehell");
+            return Task.CompletedTask;
+        }
+        public async Task SendMessage(Message message)
+        {
+            try
+            {
+                //MessageBox.Show("Message Sent", "Sent");
+                KaraManagerContext context = new KaraManagerContext();
+                context.Messages.Add(message);
+                context.SaveChanges();
+                await Clients.All.SendAsync("ReceiveKaraMessage",message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to send message","An error occurred");
+            }
+            
+            
+        }
+    }
+}
